e61198425984b5980d49648bab44f5d6bee1a074
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2024-07-17 16:24:09 +0900

    Slf4j로 DEBUG level로그 확인

--- src/main/java/kr/re/kitri/springposts/controller/PostController.java
+++ src/main/java/kr/re/kitri/springposts/controller/PostController.java
@@ -2,12 +2,17 @@ package kr.re.kitri.springposts.controller;
 
 import kr.re.kitri.springposts.model.Post;
 import kr.re.kitri.springposts.service.PostService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class PostController {
+    //private static final Logger log= LoggerFactory.getLogger(PostController.class);
     private final PostService postService;
 
     public PostController(PostService postService) {
@@ -16,19 +21,20 @@ public class PostController {
 
     @GetMapping("/posts")
     public List<Post> allPosts(){
+        log.debug("전체보기 Log");
         return postService.viewAllPosts();
     }
 
 
     @GetMapping("/posts/{postId}")
     public Post viewPostById(@PathVariable long postId){
-        System.out.println(postId);
+        log.debug("상세보기 Log");
         return postService.viewPostById(postId);
     }
 
     @PostMapping("/posts")
     public String addPost(@RequestBody Post post){
-        System.out.println(post);
+        log.debug("새로운 글 추가 Log");
         postService.registerPost(post);
         return "글 등록 성공";
 
@@ -36,6 +42,7 @@ public class PostController {
 
     @PatchMapping("/posts/{postId}/likes")
     public String doLike(@PathVariable long postId){
+        log.debug("좋아요 반영 Log");
         postService.updateLikesPlusOne(postId);
         return "좋아요 반영 성공";
     }
--- src/main/java/kr/re/kitri/springposts/model/Post.java
+++ src/main/java/kr/re/kitri/springposts/model/Post.java
@@ -1,61 +1,14 @@
 package kr.re.kitri.springposts.model;
 
+import lombok.*;
+
+@Data//getter,setter,toString,hashCode 등
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
     private long id;
     private String title;
     private String body;
     private int likes;
 
-    public Post() {
-    }
-
-    public Post(long id, String title, String body, int likes) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.likes = likes;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    //logging 용도
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                ", likes=" + likes +
-                '}';
-    }
 }
