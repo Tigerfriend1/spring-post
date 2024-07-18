2dc646627a7bdfb4423f454c17548159f1a2c420
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2024-07-18 11:33:25 +0900

    ResponseEntity<>로 wrapping하여 반환

--- src/main/java/kr/re/kitri/springposts/controller/PostController.java
+++ src/main/java/kr/re/kitri/springposts/controller/PostController.java
@@ -4,6 +4,7 @@ import kr.re.kitri.springposts.model.Post;
 import kr.re.kitri.springposts.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +17,29 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public Iterable<Post> allPosts(){
+    public ResponseEntity<Iterable<Post>> allPosts(){
         log.debug("전체보기 Log");
-        return postService.viewAllPosts();
+        return ResponseEntity.ok().body(postService.viewAllPosts());
+        //return postService.viewAllPosts();
     }
 
 
     @GetMapping("/posts/{postId}")
-    public Post viewPostById(@PathVariable long postId){
+    public ResponseEntity<Post> viewPostById(@PathVariable long postId){
         log.debug("상세보기 Log");
-        return postService.viewPostById(postId);
+        return ResponseEntity.ok().body(postService.viewPostById(postId));
     }
 
     @PostMapping("/posts")
-    public Post addPost(@RequestBody Post post){
+    public ResponseEntity<Post> addPost(@RequestBody Post post){
         log.debug("새로운 글 추가 Log");
-        return postService.registerPost(post);
+        return ResponseEntity.status(201).body(postService.registerPost(post));
     }
 
     @PatchMapping("/posts/{postId}/likes")
-    public String doLike(@PathVariable long postId){
+    public ResponseEntity<String> doLike(@PathVariable long postId){
         log.debug("좋아요 반영 Log");
         postService.updateLikesPlusOne(postId);
-        return "좋아요 반영 성공";
+        return ResponseEntity.ok().body("좋아요 반영 성공");
     }
 }
